25bcfcfc2d1601637b7cdd66c61d70215a3c9f50
YourGitHubUsername <your.email@example.com>
2025-11-13 06:09:29 +0100

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,64 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student: Student = {
+  firstName: "Innocent",
+  lastName: "Okafor",
+  age: 28,
+  location: "Nigeria"
+};
+
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const Student1: Student = {
+  firstName: "Michael",
+  lastName: "Nwaigwe",
+  age: 29,
+  location: "Ghana"
+};
+
+const Student2: Student = {
+  firstName: "Joseph",
+  lastName: "Kama",
+  age: 34,
+  location: "Congo"
+};
+
+const studentList: Student[] = [Student1 , Student2];
+
+// Table Element
+const table: HTMLTableElement = document.createElement("table");
+table.border = "1";
+
+const header: HTMLTableRowElement = table.insertRow();
+const th1: HTMLTableCellElement = document.createElement("th");
+th1.textContent = "First Name";
+const th2: HTMLTableCellElement = document.createElement("th");
+th2.textContent = "Location";
+
+header.appendChild(th1);
+header.appendChild(th2);
+
+studentList.forEach((student: Student) => {
+  const row: HTMLTableRowElement = table.insertRow();
+
+  const cell1: HTMLTableCellElement = row.insertCell();
+  cell1.textContent = student.firstName;
+
+  const cell2: HTMLTableCellElement = row.insertCell();
+  cell2.textContent = student.location;
+});
+
+document.body.appendChild(table);
+
+
