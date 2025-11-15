6b5a4c7907207961239c1978d76ac513d821cfad
YourGitHubUsername <your.email@example.com>
2025-11-15 18:15:38 +0100

    Complete task_4: Namespace & Declaration Merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,21 @@
+/// <reference path="./Teacher.ts" />
+
+namespace Subjects {
+  // Merge: Add experienceTeachingJava
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingJava === undefined || this.teacher.experienceTeachingJava === 0) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
